9b1178e23f66b41007cfb2d28d273a118716db77
Anna Oh <annaoh1994@gmail.com>
2017-11-23 15:11:02 -0800

    Reformatting some code to be more readable

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router';
+import { Link, withRouter } from 'react-router-dom';
+
 
 class Greeting extends React.Component {
 
